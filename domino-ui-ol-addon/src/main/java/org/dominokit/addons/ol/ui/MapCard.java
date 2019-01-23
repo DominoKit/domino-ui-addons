800688e74dbcb6c1379a87e370b5841676c2e77c
Tino Desjardins <tino.desjardins@arcor.de>
2019-01-23 12:24:55 +0100

    Add map card component

---
+++ domino-ui-ol-addon/src/main/java/org/dominokit/addons/ol/ui/MapCard.java
@@ -0,0 +1,40 @@
+package org.dominokit.addons.ol.ui;
+
+import org.dominokit.domino.ui.cards.Card;
+import org.dominokit.domino.ui.utils.BaseDominoElement;
+import elemental2.dom.HTMLDivElement;
+
+/**
+ * TODO Use inheritance when static factory methods of {@link Card} are removed.
+ */
+public class MapCard extends BaseDominoElement<HTMLDivElement, MapCard> {
+
+    private Card mapCard;
+
+    public MapCard(String mapId) {
+        this(mapId, null);
+    }
+
+    public MapCard(String mapId, String title) {
+
+        this.mapCard = new Card();
+
+        if (title != null) {
+            this.mapCard.setTitle(title);
+        }
+
+        init(this);
+        this.mapCard.fitContent();
+        this.mapCard.getBody().setId(mapId);
+    }
+
+    @Override
+    public HTMLDivElement asElement() {
+        return this.mapCard.asElement();
+    }
+
+    public final Card getCard() {
+        return this.mapCard;
+    }
+
+}
